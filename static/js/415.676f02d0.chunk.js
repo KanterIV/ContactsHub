f495e4ae2c21dd10f4dcb8a5b59abee5045248fd
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-11-06 20:45:34 +0000

    Deploying to gh-pages from @ KanterIV/goit-react-hw-08-phonebook@79eeb9d507af03d3e28585e6d5a905e6e21313f5 🚀

---
+++ static/js/415.676f02d0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,o){o.r(t);o(2791);var a=o(184);t.default=function(){return(0,a.jsx)("div",{children:"Welcome to my contact book app. If you want to use the service, please register and log in. Good luck!"})}}}]);
+//# sourceMappingURL=415.676f02d0.chunk.js.map
